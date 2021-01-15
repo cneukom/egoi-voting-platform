593cb58343e3518a9ab35f14d3913b97744da28a
Cédric Neukom <gitlab@cedl.ch>
2021-01-15 16:59:48 +0100

    Dummy Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,10 +2,38 @@
 
 namespace Database\Seeders;
 
+use App\Models\Contestant;
+use App\Models\Delegation;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
+    private function makeContestant(Delegation $delegation, int $idx): Contestant
+    {
+        $contestant = new Contestant();
+        $contestant->name = 'Sample ' . $delegation->name . (1 + $idx);
+        $contestant->code = $delegation->code . $idx;
+        $contestant->access_token = Str::random(64);
+        $contestant->delegation()->associate($delegation);
+        $contestant->save();
+        return $contestant;
+    }
+
+    private function makeDelegation(string $name, string $code): Delegation
+    {
+        $delegation = new Delegation();
+        $delegation->code = $code;
+        $delegation->name = $name;
+        $delegation->access_token = Str::random(64);
+        $delegation->save();
+
+        for ($i = 0; $i < 4; $i++) {
+            $this->makeContestant($delegation, $i);
+        }
+        return $delegation;
+    }
+
     /**
      * Seed the application's database.
      *
@@ -13,6 +41,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->makeDelegation('Switzerland', 'CH');
+        $this->makeDelegation('Italy', 'IT');
     }
 }
