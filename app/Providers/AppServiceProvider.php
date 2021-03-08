27e06833f2b90d580545969dd27a29205254e2f8
Cédric Neukom <gitlab@cedl.ch>
2021-03-08 21:26:26 +0100

    Adapt dependencies

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Aws\S3\S3Client;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,17 +13,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->when(S3Client::class)
-            ->needs('$args')
-            ->give(fn() => [
-                'endpoint' => config('filesystems.disks.s3.endpoint'),
-                'version' => 'latest',
-                'region' => config('filesystems.disks.s3.region'),
-                'credentials' => [
-                    'key' => config('filesystems.disks.s3.key'),
-                    'secret' => config('filesystems.disks.s3.secret'),
-                ],
-            ]);
     }
 
     /**
