20b267234fcc130c9785896a5352f13284b4f6ad
Cédric Neukom <cedric@egoi.ch>
2021-03-29 16:32:33 +0200

    Fix typo in notification

--- resources/lang/en/notifications.php
+++ resources/lang/en/notifications.php
@@ -2,6 +2,6 @@
 
 return [
     'user_import' => [
-        'welcome' => 'Today\'s votes will be held on the EGOI voting platform. Your delegation gets exactly one vote per ballot. Use the following link to sing in:' . PHP_EOL . ':url',
+        'welcome' => 'Today\'s votes will be held on the EGOI voting platform. Your delegation gets exactly one vote per ballot. Use the following link to sign in:' . PHP_EOL . ':url',
     ],
 ];
